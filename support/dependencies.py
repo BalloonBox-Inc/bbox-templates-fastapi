c6a35474f4b3118c4b7082bdb92ba0ca36207198
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-12 11:04:28 -0700

    Update dependencies.py

--- support/dependencies.py
+++ support/dependencies.py
@@ -13,7 +13,7 @@ async def verify_admin_token(admin_token: str = Header()):
 
 
 async def verify_user_token(user_token: str = Header()):
-    if user_token != getenv('USER_TOKEN'):
+    if user_token != 'fake-super-secret-token':
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Invalid user token'
