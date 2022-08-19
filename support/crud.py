ceca9e9badc75c2edf7ec84127c5f5085da25b00
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-19 00:41:24 -0700

    Update crud.py

--- support/crud.py
+++ support/crud.py
@@ -1,5 +1,22 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
-from support import models
+from support.hashing import encrypt_password
+from support import models, schemas
+
+
+def create_admin_user(db: Session, item: schemas.AdminCreate):
+    new_admin_user = models.AdminTable(email=item.email, hashed_password=encrypt_password(item.password))
+    try:
+        db.add(new_admin_user)
+        db.commit()
+        db.refresh(new_admin_user)
+    except Exception as e:
+        db.rollback()
+        db.close()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=str(e))
+    return new_admin_user
 
 
 def get_user_by_email(db: Session, email: str):
