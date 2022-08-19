c226f4ac4a5ab38995415c3d082bd2e7949c167c
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-18 21:31:16 -0700

    Update schemas.py

--- support/schemas.py
+++ support/schemas.py
@@ -12,16 +12,3 @@ class UserBase(BaseModel):
     id: str
     email: str
     password: str
-
-
-# database classes
-class UserCreate(UserBase):
-    password: str
-
-
-class User(UserBase):
-    id: int
-    is_active: bool
-
-    class Config:
-        orm_mode = True
