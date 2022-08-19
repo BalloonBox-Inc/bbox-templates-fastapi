632561d132bb638689b5434bb2051c0dc88491b0
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-19 00:26:30 -0700

    Update crud.py

--- support/crud.py
+++ support/crud.py
@@ -6,24 +6,12 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.UserTable).filter(models.UserTable.email == email).first()
 
 
-# fetch row by column value
-def get_row_by_column(db: Session, value: str):
-    return db.query(models.UserTable).filter(models.UserTable.email == value).first()
-
-
-# fetch row where column is empty
-def get_row_by_column_null(db: Session):
-    return db.query(models.UserTable).filter(models.UserTable.email == None).order_by(models.UserTable.email.asc()).first()
-
-
-# add new row
 def add_row(db: Session, obj: models.UserTable):
     db.add(obj)
     db.commit()
     return
 
 
-# update existent row
 def update_row(db: Session, obj: models.UserTable):
     db.flush()
     db.commit()
