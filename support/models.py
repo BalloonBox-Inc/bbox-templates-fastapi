8f55067e91401ca6249b4fac5e314694378f1247
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-18 23:22:08 -0700

    Update models.py

--- support/models.py
+++ support/models.py
@@ -1,16 +1,5 @@
-from sqlalchemy import Column, Boolean, Integer, Float, String, DateTime, null
+from sqlalchemy import Column, Boolean, Integer, Float, String, DateTime
 from support.database import Base
-
-
-# database table classes
-class AdminTable(Base):
-
-    __tablename__ = 'admin_test'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
 
 
 class UserTable(Base):
@@ -18,6 +7,7 @@ class UserTable(Base):
     __tablename__ = 'users_test'
 
     id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
