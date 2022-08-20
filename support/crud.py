c389ebb515dc056535cba9f132db13afe622d473
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-20 16:55:01 -0700

    Update crud.py

--- support/crud.py
+++ support/crud.py
@@ -4,37 +4,17 @@ from sqlalchemy import exc
 from support import models, schemas, hashing
 
 
-def create_admin_user(db: Session, item: schemas.AdminCreate):
-    new_admin_user = models.AdminTable(
-        email=item.email, hashed_password=hashing.encrypt_password(item.password))
-    try:
-        db.add(new_admin_user)
-        db.commit()
-        db.refresh(new_admin_user)
-    except Exception as e:
-        db.rollback()
-        db.close()
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=str(e))
-    return new_admin_user
-
-
-def get_user_by_email(db: Session, email: str):
-    return db.query(models.UserTable).filter(models.UserTable.email == email).first()
-
-
 def add_row(db: Session, obj: models.UserTable):
     try:
         db.add(obj)
         db.commit()
         return
-    except exc.SQLAlchemyError:
+    except exc.SQLAlchemyError as e:
         db.rollback()
         db.close()
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail='Unable to add data into database')
+            detail='Unable to add data into database') from e
 
 
 def update_row(db: Session, obj: models.UserTable):
@@ -42,9 +22,19 @@ def update_row(db: Session, obj: models.UserTable):
         db.flush()
         db.commit()
         return
-    except exc.SQLAlchemyError:
+    except exc.SQLAlchemyError as e:
         db.rollback()
         db.close()
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail='Unable to update data to database')
+            detail='Unable to update data to database') from e
+
+
+def create_admin_user(db: Session, item: schemas.AdminCreate):
+    new_admin_user = models.AdminTable(
+        email=item.email, hashed_password=hashing.encrypt_password(item.password))
+    return add_row(db, new_admin_user)
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.UserTable).filter(models.UserTable.email == email).first()
