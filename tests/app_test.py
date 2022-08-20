f466387f2dd132804fd9e3586e9e895efcb9d502
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-20 15:45:31 -0700

    Update app_test.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,16 +1,2 @@
-# from tests.api_testing.example import ExampleTest
-from tests.unit_testing.example import ExampleTest
-import unittest
-
-
-def suite():
-
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ExampleTest))
-
-    return suite
-
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
+from tests.api_testing.routers_example import *
+from tests.unit_testing.example import *
