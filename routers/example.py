5408a12b42f85173bf7de3a828cabf9d5c3b6f98
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-20 13:58:58 -0700

    Update example.py

--- routers/example.py
+++ routers/example.py
@@ -1,45 +1,73 @@
 from fastapi import APIRouter, Depends, Request, HTTPException, status
 from sqlalchemy.orm import Session
 from support.database import get_db
-from support.dependencies import get_current_active_user
-from support import crud, models, schemas
+from support import crud, dependencies, hashing, models, schemas
 
 
 router = APIRouter(
-    prefix='/example',
-    tags=['Example'])
+    prefix='/user',
+    tags=['User'])
 
 
-@router.post('/', status_code=status.HTTP_200_OK, summary='Example router')
-async def example_router(request: Request, item: schemas.UserBase = Depends(get_current_active_user), db: Session = Depends(get_db)):
+@router.post('/create', status_code=status.HTTP_200_OK)
+async def create_user(request: Request, item: schemas.UserBase, db: Session = Depends(get_db)):
     '''
     Description
 
     Inputs:
-    - **variable_name [dtype]**: description
+    - **email [str]**: user email
+    - **password [str]**: user password
 
     Outputs:
-    - **variable_name [dtype]**: description
+    - **[object]**: {
+        'error': False,
+        'detail': 'User has successfully been created'
+        }
     '''
 
     print(f'\033[35;1m Request received from: {request.client.host}\033[0m')
 
-    # get user by email
+    # check if user exists
     user = crud.get_user_by_email(db, email=item.email)
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='Email already registered')
 
-    # update user
-    user.password = 'password'
-    crud.update_row(db, obj=user)
-
     # add user
     crud.add_row(
         db, models.UserTable(
-            email='email@example.com',
-            hashed_password='encrypted_password',
+            email=item.email,
+            hashed_password=hashing.encrypt_password(item.password),
             is_active=True))
 
-    return {'error': False}
+    return {'error': False, 'detail': 'User has successfully been created'}
+
+
+@router.post('/update', status_code=status.HTTP_200_OK)
+async def update_user(request: Request, item: schemas.UserUpdate, db: Session = Depends(get_db)):
+    '''
+    Description
+
+    Inputs:
+    - **email [str]**: user email
+    - **password [str]**: user password
+    - **is_active [bool]**: user status
+
+    Outputs:
+    - **[object]**: {
+        'error': False,
+        'detail': 'User has successfully been updated'
+        }
+    '''
+
+    print(f'\033[35;1m Request received from: {request.client.host}\033[0m')
+
+    # authenticate user
+    user = dependencies.authenticate_user(db, email=item.email, password=item.password)
+
+    # update user
+    user.is_active = item.is_active
+    crud.update_row(db, obj=user)
+
+    return {'error': False, 'detail': 'User has successfully been updated'}
