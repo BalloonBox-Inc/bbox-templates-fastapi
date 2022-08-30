f4218b2a35856fbfeb6ab14777f7434f675e7c13
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-08-30 10:19:22 -0700

    Update apis

--- apis/routers.py
+++ apis/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from apis import example
+from apis import user
 
 
 api_routers = APIRouter()
-api_routers.include_router(example.router, prefix='/example', tags=['example'])
+api_routers.include_router(user.router, prefix='/user', tags=['users'])
