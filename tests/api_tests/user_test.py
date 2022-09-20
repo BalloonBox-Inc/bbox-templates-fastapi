41135edcb1e2794557babb58e87f81d015e0020e
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-09-20 12:11:38 -0700

    Update user_test.py

--- tests/api_tests/user_test.py
+++ tests/api_tests/user_test.py
@@ -6,20 +6,6 @@ from main import app
 client = TestClient(app)
 
 
-def test_create_user():
-
-    response = client.post(
-        url='/user/create',
-        json={
-            'email': settings.TEST_USER_EMAIL,
-            'password': settings.TEST_USER_PASSWORD})
-
-    assert response.status_code == 200
-    assert response.json() == {
-        'error': False,
-        'detail': 'User has successfully been created'}
-
-
 def test_update_user():
 
     response = client.post(
